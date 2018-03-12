d94d10eba4d00545a61aee28429a372e33e872e0
Jake 'Sid' Smith <jake@dabapps.com>
2018-03-12 11:27:17 +0000

    Add more examples

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,20 +1,36 @@
 import * as React from 'react';
+import { Component, HTMLProps } from 'react';
 import * as ReactDOM from 'react-dom';
 
 interface ChildProps {
   name: string;
 }
 
+interface TestProps extends HTMLProps<HTMLDivElement> {
+  className: string;
+}
+
 const Child = ({ name }: ChildProps) => <p>Hello, {name}!</p>;
 
-class Test extends React.Component {
+class Test extends Component<TestProps, {}> {
   public render() {
     return (
-      <div className="test" style={{ width: 100 }}>
+      <div className="test" style={{ width: 100 }} onClick={this.boundMethod}>
         <Child name="World" />
+        {[
+          <div key={0} />,
+          <div key={1} />,
+          <div key={2} />,
+          <div key={3} />,
+          <div key={4} />,
+        ]}
       </div>
     );
   }
+
+  private boundMethod = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
 }
 
-ReactDOM.render(<Test />, document.getElementById('test'));
+ReactDOM.render(<Test className={'test'} />, document.getElementById('test'));
